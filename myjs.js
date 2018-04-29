3bd4f0145b4f60655c6b556e2fb36fb650c0d905
shadab14meb346 <31199288+shadab14meb346@users.noreply.github.com>
2018-04-29 23:22:54 +0530

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,95 @@
+ if (typeof web3 !== 'undefined') {
+            web3 = new Web3(web3.currentProvider);
+        } else {
+            // set the provider you want from Web3.providers
+            web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+     }
+
+     web3.eth.defaultAccount = web3.eth.accounts[0];
+     var LotteryContract = web3.eth.contract([
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "enter",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "pickWinner",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getPlayers",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "manager",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "players",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]);
+var Lottery = LotteryContract.at("0x4bea2866a76b614efc1e2dad190a9719a1b4efc8");
+$("#button").click(function(){
+	x=$("#ether").val();
+	$("#loader").show();
+	var x=$("#ether").val()
+	Lottery.enter(x,(err,res)=>{
+		if(err)
+			$("#loader").hide();
+	});
+});
